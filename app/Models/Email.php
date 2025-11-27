1c47de585a8c35ffe3d5ac8b2e382b92db1b9d84
akzize <akzakariae@gmail.com>
2025-11-27 12:25:15 +0100

    add the RickDBCN\FilamentEmail migrations and configs and register it

---
+++ app/Models/Email.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use RickDBCN\FilamentEmail\Models\Email as RickEmail;
+class Email extends RickEmail
+{
+    //
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -56,6 +56,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->plugin(\RickDBCN\FilamentEmail\FilamentEmail::make());
     }
 }
