b2a4e1bdaa64869eb2b02fb6d45d7b21e049d160
akzize <akzakariae@gmail.com>
2025-11-27 12:31:40 +0100

    refactor web routes and format it

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Laravel\Fortify\Features;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+/* ------------------------------- news routes ------------------------------ */
 Route::get('/news', [NewsController::class, 'index'])->name('news');
 
 Route::get('/news/{id}', function ($id) {
@@ -23,26 +24,40 @@ Route::get('/news/{id}', function ($id) {
     ]);
 });
 
+/* -------------------------------------------------------------------------- */
+/*                                events routes                               */
+/* -------------------------------------------------------------------------- */
 Route::get('/events', [EventController::class, 'index'])->name('events');
 
 Route::get('/events/{id}', [EventController::class, 'show']);
 
+// gallery routes
 Route::get('/gallery', [AlbumController::class, 'index'])->name('gallery');
 
+// about routes
 Route::get('/about', function () {
     return Inertia::render('About', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('about');
 
+/* ---------------------------- contact us routes --------------------------- */
 Route::get('/contact', function () {
     return Inertia::render('Contact', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('contact');
 
+/* ---------------------------- dashboard routes --------------------------- */
 Route::post('/email-send', [EmailController::class, 'store'])->name('email-send');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+/* ---------------------- default route for 404 errors ---------------------- */
 Route::fallback(function(){
     return Inertia::render('NotFound');
 });
